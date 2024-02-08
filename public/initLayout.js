e9d7aa2e25e23ed51a1d9cd51078adb80d3accda
TheSaviourEking <saviour.eking@outlook.com>
2024-02-08 01:13:44 +0100

    [chore]: implement phase 1: the UI and passed all tests

--- public/initLayout.js
+++ public/initLayout.js
@@ -1,14 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const GAMELENGTH = 3;
+    const BOARDLENGTH = 3;
     const h1 = document.createElement('h1');
     h1.setAttribute('class', 'heading');
-    h1.innerText = 'Winner:';
+    // h1.innerText = 'Winner:';
     // const heading = `<h1 class='heading'>Winner:</h1>`;
 
     const gameContainer = document.createElement('div');
     gameContainer.setAttribute('class', 'game-container');
-    for (let row = 0; row < GAMELENGTH; row++) {
-        for (let col = 0; col < GAMELENGTH; col++) {
+    for (let row = 0; row < BOARDLENGTH; row++) {
+        for (let col = 0; col < BOARDLENGTH; col++) {
             const div = document.createElement('div');
             div.setAttribute('class', 'cell');
             div.setAttribute('data-row', row);
@@ -17,13 +17,18 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    for (let i = 0; i < BOARDLENGTH * BOARDLENGTH; i++) {
+        const div = gameContainer.childNodes[i];
+        div.setAttribute('id', `square-${i}`);
+    }
+
     const controls = document.createElement('div');
-    controls.setAttribute('class', 'controls')
+    controls.setAttribute('class', 'controls');
     const newGameControl = document.createElement('button');
     const giveUpControl = document.createElement('button');
 
     newGameControl.innerText = 'New Game';
-    giveUpControl.innerText = 'Give Up'
+    giveUpControl.innerText = 'Give Up';
     controls.appendChild(newGameControl);
     controls.appendChild(giveUpControl);
 
--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,1 +1,19 @@
 // Your code here
+function init() {
+    const gameContainer = document.getElementsByClassName('game-container')[0];
+    gameContainer.addEventListener('click', gameContainerClickHandler);
+
+    /* gameContainer click */
+
+
+}
+
+let turn = 'o';
+function switchTurn() {
+    turn === 'x' ? turn = 'o' : turn = 'x';
+    return turn;
+}
+
+
+
+window.onload = init;
